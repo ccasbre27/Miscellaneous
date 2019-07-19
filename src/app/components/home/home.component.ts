bdadb08163d8cc5faa1d05677e43171a52b71a15
ccasbre27@hotmail.com <ccasbre27@hotmail.com>
2019-07-19 11:08:45 -0600

    added an example of parent and children routes

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+  
+  <app-ng-style></app-ng-style>
+  
+  <app-css></app-css>
+  
+  <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium consequuntur itaque placeat officiis hic eveniet, reprehenderit earum molestiae nihil expedita quam aliquam sint minus. Suscipit minima ipsum error repudiandae possimus.
+  </p>
+  
+  <app-clases></app-clases>
+  
+  <p [appResaltado]="'orange'">
+      Hola Mundo
+  </p>
+  
+  <app-ng-switch></app-ng-switch>
+  
+  
+
+  `,
+  styles: []
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
